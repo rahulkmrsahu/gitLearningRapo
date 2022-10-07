5018de8a0f2d0a265048600e7f015ab04c23eba5
rahulkmrsahu <rahulkmrsahu@gmail.com>
2022-10-07 09:30:18 +0530

    Sum of two numbers

---
+++ Sum/AddTwoNos.java
@@ -0,0 +1,11 @@
+package Sum;
+
+public class AddTwoNos {
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        int sum=0;
+        sum=a+b;
+        System.out.println("sum="+sum);
+    }
+}
